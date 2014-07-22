3f00ed792e2b740417333fba206bcd21422dde1f
youpu <youpu@youpudeiMac.local>
2014-07-22 16:53:29 +0800

    Initial Commit

---
+++ 游谱旅行/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  游谱旅行
+//
+//  Created by youpu on 14-7-22.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
